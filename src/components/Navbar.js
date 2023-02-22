493a761c9b21e556db97d14f34b05113bddd1886
AndreD16 <107078683+AndreD16@users.noreply.github.com>
2023-02-22 13:03:34 -0500

    1 file modified, 2 added, 2 deleted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/react-icon-small.png" className="nav--icon" />
+      <img
+        src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
+        className="nav--icon"
+      />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
